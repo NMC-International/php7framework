429166a9382324f7d7d82fe594cd988c283a71c7
Engr. Syed Rowshan Ali <engr.rowshan@gmail.com>
2019-04-15 16:37:12 +0600

    Error Class Commented

--- core/Error.php
+++ core/Error.php
@@ -1,14 +1,17 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: rowshan
+ * User: Engr. Syed Rowshan Ali
  * Date: 15-Apr-19
  * Time: 2:15 PM
  */
 
-// A singleton class to handle request.
+/** A singleton class to handle Error generated into the page.
+ *  This class will be available to the controller
+ *
+*/
 class Error {
-	private static $instance = null;
+	private static $instance = null;    //Singleton Instance
 
 	private function __construct()
 	{
@@ -25,6 +28,10 @@ class Error {
 		return self::$instance;
 	}
 
+	/**
+	 * @param null $msg
+	 * Show 404 Error Page
+	 */
 	public function show_404($msg = null){
 		header('HTTP/1.1 404 Not Found');
 		echo $msg != null ? 'Error:404 :: ' .$msg :'Page not found #404';
